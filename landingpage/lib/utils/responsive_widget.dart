5f98bb7642ea4b44e0db1ab5e24c59e0cde4ca2a
ptyagicodecamp <ptyagicodecamp@gmail.com>
2019-06-10 16:32:39 -0700

    make web landing page responsive

---
+++ landingpage/lib/utils/responsive_widget.dart
@@ -0,0 +1,46 @@
+import 'package:flutter_web/material.dart';
+
+class ResponsiveWidget extends StatelessWidget {
+  final Widget largeScreen;
+  final Widget mediumScreen;
+  final Widget smallScreen;
+
+  const ResponsiveWidget(
+      {Key key, this.largeScreen, this.mediumScreen, this.smallScreen})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //Returns the widget which is more appropriate for the screen size
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxWidth > 1200) {
+        return largeScreen;
+      } else if (constraints.maxWidth > 800 && constraints.maxWidth < 1200) {
+        //if medium screen not available, then return large screen
+        return mediumScreen ?? largeScreen;
+      } else {
+        //if small screen implementation not available, then return large screen
+        return smallScreen ?? largeScreen;
+      }
+    });
+  }
+
+  //Making these methods static, so that they can be used as accessed from other widgets
+
+  //Large screen is any screen whose width is more than 1200 pixels
+  static bool isLargeScreen(BuildContext context) {
+    return MediaQuery.of(context).size.width > 1200;
+  }
+
+  //Small screen is any screen whose width is less than 800 pixels
+  static bool isSmallScreen(BuildContext context) {
+    return MediaQuery.of(context).size.width < 800;
+  }
+
+  //Medium screen is any screen whose width is less than 1200 pixels,
+  //and more than 800 pixels
+  static bool isMediumScreen(BuildContext context) {
+    return MediaQuery.of(context).size.width > 800 &&
+        MediaQuery.of(context).size.width < 1200;
+  }
+}
