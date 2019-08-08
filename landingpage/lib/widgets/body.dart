9a56b230128bceb708c0f2bf01236a9b1b12351b
ptyagicodecamp <ptyagicodecamp@gmail.com>
2019-08-08 15:04:36 -0700

    working on flutter facts

--- landingpage/lib/widgets/body.dart
+++ landingpage/lib/widgets/body.dart
@@ -1,9 +1,10 @@
+import 'package:flutter_dialogflow/dialogflow_v2.dart';
 import 'package:flutter_web/material.dart';
 import 'package:landingpage/utils/strings.dart';
 import 'package:landingpage/utils/myColors.dart';
 import 'package:landingpage/widgets/emailBox.dart';
 import 'package:landingpage/utils/responsive_widget.dart';
-import 'package:landingpage/utils/themes.dart';
+import 'package:landingpage/widgets/message.dart';
 
 var backgroundImage = "assets/flutter_image.jpg";
 
@@ -17,15 +18,94 @@ class Body extends StatelessWidget {
   }
 }
 
-class LargeScreen extends StatelessWidget {
+class LargeScreen extends StatefulWidget {
+  LargeScreen({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _LargeScreenState createState() => new _LargeScreenState();
+}
+
+class _LargeScreenState extends State<LargeScreen> {
+  final List<FactsMessage> _messages = <FactsMessage>[];
+  final TextEditingController _textController = new TextEditingController();
+
+  Widget _buildTextComposer(BuildContext context) {
+    return new IconTheme(
+      data: new IconThemeData(color: Theme.of(context).accentColor),
+      child: new Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: new Row(
+          children: <Widget>[
+            new Flexible(
+              child: new TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration:
+                new InputDecoration.collapsed(hintText: "Send a message"),
+              ),
+            ),
+            new Container(
+              margin: new EdgeInsets.symmetric(horizontal: 4.0),
+              child: new IconButton(
+                  icon: new Icon(Icons.send),
+                  onPressed: () => _handleSubmitted(_textController.text)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void Response(query) async {
+    _textController.clear();
+    AuthGoogle authGoogle = await AuthGoogle(
+        fileJson: "assets/flutter-to-fly-creds.json")
+        .build();
+    Dialogflow dialogflow =
+    Dialogflow(authGoogle: authGoogle, language: Language.english);
+    AIResponse response = await dialogflow.detectIntent(query);
+    FactsMessage message = new FactsMessage(
+      text: response.getMessage() ??
+          new CardDialogflow(response.getListMessage()[0]).title,
+      name: "Bot",
+      type: false,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+    FactsMessage message = new FactsMessage(
+      text: text,
+      name: "Promise",
+      type: true,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+    Response(text);
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 600,
       child: Stack(
         fit: StackFit.expand,
-        children: <Widget>[addBackground(), addWelcomeText(context)],
+        children: <Widget>[addBackground(), addWelcomeText(context), factBot(context)],
       ),
+    );
+  }
+
+  Widget factBot(BuildContext context) {
+    return FloatingActionButton(
+      elevation: 4.0,
+      backgroundColor: Colors.blueAccent,
+      onPressed: () => _buildTextComposer,
     );
   }
 
---
+++ landingpage/lib/widgets/message.dart
@@ -0,0 +1,63 @@
+import 'package:flutter_web/material.dart';
+
+class FactsMessage extends StatelessWidget {
+  FactsMessage({this.text, this.name, this.type});
+
+  final String text;
+  final String name;
+  final bool type;
+
+  List<Widget> otherMessage(context) {
+    return <Widget>[
+      new Container(
+        margin: const EdgeInsets.only(right: 16.0),
+        child: new CircleAvatar(child: new Image.asset("images/rad5hub_1.png")),
+      ),
+      new Expanded(
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Text(this.name,
+                style: new TextStyle(fontWeight: FontWeight.bold)),
+            new Container(
+              margin: const EdgeInsets.only(top: 5.0),
+              child: new Text(text),
+            ),
+          ],
+        ),
+      ),
+    ];
+  }
+
+  List<Widget> myMessage(context) {
+    return <Widget>[
+      new Expanded(
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            new Text(this.name, style: Theme.of(context).textTheme.subhead),
+            new Container(
+              margin: const EdgeInsets.only(top: 5.0),
+              child: new Text(text),
+            ),
+          ],
+        ),
+      ),
+      new Container(
+        margin: const EdgeInsets.only(left: 16.0),
+        child: new CircleAvatar(child: new Text(this.name[0])),
+      ),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: new Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: this.type ? myMessage(context) : otherMessage(context),
+      ),
+    );
+  }
+}
