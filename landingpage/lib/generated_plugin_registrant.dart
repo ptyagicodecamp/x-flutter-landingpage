b7839fa80c5da39f19e15ad5b897bdac4f7a74cb
ptyagicodecamp <ptyagicodecamp@gmail.com>
2020-04-10 23:11:41 -0700

    added desktop application support

---
+++ landingpage/lib/generated_plugin_registrant.dart
@@ -0,0 +1,18 @@
+//
+// Generated file. Do not edit.
+//
+
+// ignore: unused_import
+import 'dart:ui';
+
+import 'package:google_sign_in_web/google_sign_in_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+// ignore: public_member_api_docs
+void registerPlugins(PluginRegistry registry) {
+  GoogleSignInPlugin.registerWith(registry.registrarFor(GoogleSignInPlugin));
+  UrlLauncherPlugin.registerWith(registry.registrarFor(UrlLauncherPlugin));
+  registry.registerMessageHandler();
+}
